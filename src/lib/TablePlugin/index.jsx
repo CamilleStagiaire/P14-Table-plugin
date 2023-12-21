6256a7127bf25847ab1459d3185487d8e43e58fc
CamilleStagiaire <100408607+CamilleStagiaire@users.noreply.github.com>
2023-12-21 15:53:49 +0100

    tri, search, pagination

--- src/lib/TablePlugin/index.jsx
+++ src/lib/TablePlugin/index.jsx
@@ -1,9 +1,16 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import chevronUp from "./assets/chevronUp.svg";
 import chevronDown from "./assets/chevronDown.svg";
 import "./style.css";
 
-const TablePlugin = ({ data, dataMapping }) => {
+const TablePlugin = ({ 
+  data,
+  dataMapping,
+  oddRowColor = "rgba(90, 111, 8, 0.1)",
+  sortedColumnColor = "rgba(90, 111, 8, 0.1)",
+  selectPaginationColor = "rgba(90, 111, 8, 0.9)",
+
+ }) => {
   const [sortConfig, setSortConfig] = useState({
     key: "lastName",
     direction: "ascending",
@@ -70,94 +77,129 @@ const TablePlugin = ({ data, dataMapping }) => {
     setCurrentPage((prevPage) => Math.min(prevPage + 1, pageNumbers.length));
   };
 
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [entriesPerPage]);
+
   return (
- 
-      <div className="container-layout">
-        <div className="select">
-          <label>
+    <div className="container-layout">
+      <div className="select"  style={{ backgroundColor: selectPaginationColor }}>
+        <label>
           <span>Show </span>
-            <select
-              value={entriesPerPage}
-              onChange={(e) => setEntriesPerPage(Number(e.target.value))}
-            >
-              <option value={10}>10</option>
-              <option value={25}>25</option>
-              <option value={50}>50</option>
-              <option value={100}>100</option>
-            </select>
-            <span> entries</span>
-          </label>
-          <label>
+          <select
+            value={entriesPerPage}
+            onChange={(e) => setEntriesPerPage(Number(e.target.value))}
+          >
+            <option value={10}>10</option>
+            <option value={25}>25</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
+          </select>
+          <span> entries</span>
+        </label>
+        <label>
           <span>Search: </span>
-            <input
-              type="text"
-              placeholder="Search..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-          </label>
-        </div>
-        <table className="container-layout table">
-          <thead>
-            <tr className="list-header">
-              {Object.keys(dataMapping).map((key) => (
-                <th key={key} onClick={() => requestSort(key)}>
-                  {dataMapping[key]}
-                  <div className="chevron-container">
-                    <img
-                      src={chevronUp}
-                      alt="Asc"
-                      className={`ascending ${
-                        sortConfig &&
-                        sortConfig.key === key &&
-                        sortConfig.direction === "ascending"
-                          ? "active"
-                          : ""
-                      }`}
-                    />
-                    <img
-                      src={chevronDown}
-                      alt="Desc"
-                      className={`descending ${
-                        sortConfig &&
-                        sortConfig.key === key &&
-                        sortConfig.direction === "descending"
-                          ? "active"
-                          : ""
-                      }`}
-                    />
-                  </div>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {currentEntries.map((item, index) => (
-              <tr className="list-body" key={index}>
-                {Object.keys(dataMapping).map((key, cellIndex) => (
-                  <td key={cellIndex}>{item[key]}</td>
-                ))}
-              </tr>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </label>
+      </div>
+      <table className="container-layout table">
+        <thead>
+          <tr className="list-header">
+            {Object.keys(dataMapping).map((key) => (
+              <th
+                key={key}
+                onClick={() => requestSort(key)}
+                className={sortConfig.key === key ? "sorted" : ""}
+                style={{ backgroundColor: sortConfig.key === key ? sortedColumnColor : '' }}
+              >
+                {dataMapping[key]}
+                <div className="chevron-container">
+                  <img
+                    src={chevronUp}
+                    alt="Asc"
+                    className={`ascending ${
+                      sortConfig &&
+                      sortConfig.key === key &&
+                      sortConfig.direction === "ascending"
+                        ? "active"
+                        : ""
+                    }`}
+                  />
+                  <img
+                    src={chevronDown}
+                    alt="Desc"
+                    className={`descending ${
+                      sortConfig &&
+                      sortConfig.key === key &&
+                      sortConfig.direction === "descending"
+                        ? "active"
+                        : ""
+                    }`}
+                  />
+                </div>
+              </th>
             ))}
-          </tbody>
-        </table>
-        <div className="pagination">
+          </tr>
+        </thead>
+        <tbody>
+  {currentEntries.map((item, index) => (
+    <tr 
+      key={index} 
+      className="list-body" 
+      style={{ backgroundColor: index % 2 !== 0 ? oddRowColor : '' }}
+    >
+      {Object.keys(dataMapping).map((key, cellIndex) => (
+        <td
+          key={cellIndex}
+          className={sortConfig.key === key ? "sorted" : ""}
+          style={{ backgroundColor: sortConfig.key === key ? sortedColumnColor : '' }}
+        >
+          {item[key]}
+        </td>
+      ))}
+    </tr>
+  ))}
+</tbody>
+      </table>
+      <div className="pagination"  style={{ backgroundColor: selectPaginationColor }}>
         <span>
           Showing {startEntry} to {endEntry} of {totalEntries} entries
         </span>
         <div className="pages">
-          <span onClick={handlePreviousPage} disabled={currentPage === 1}>Previous </span>
+          <span
+            onClick={handlePreviousPage}
+            onKeyDown={(e) => e.key === 'Enter' && handlePreviousPage()} 
+            disabled={currentPage === 1}
+            tabIndex="0"
+          >
+            Previous{" "}
+          </span>
           {pageNumbers.map((number) => (
-            <button key={number} onClick={() => setCurrentPage(number)} className={number === currentPage ? 'active' : ''}>
+            <button
+              key={number}
+              onClick={() => setCurrentPage(number)}
+              className={number === currentPage ? "active" : ""}
+            >
               {number}
             </button>
           ))}
-          <span onClick={handleNextPage} disabled={currentPage === pageNumbers.length}> Next</span>
+          <span
+            onClick={handleNextPage}
+            onKeyDown={(e) => e.key === 'Enter' && handleNextPage()} 
+            disabled={currentPage === pageNumbers.length}
+            tabIndex="0"
+          >
+            {" "}
+            Next
+          </span>
         </div>
       </div>
-      </div>
-
+    </div>
   );
 };
-
 export default TablePlugin;
