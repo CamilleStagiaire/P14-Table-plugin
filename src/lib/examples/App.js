1470d88c52b8bcccc5134058ce46c13d84b744aa
CamilleStagiaire <100408607+CamilleStagiaire@users.noreply.github.com>
2024-01-03 18:15:12 +0100

    Mise à jour de mon package

--- src/lib/examples/App.js
+++ src/lib/examples/App.js
@@ -16,15 +16,6 @@ function App() {
     department: "Department",
   };
 
-  function generateRandomDate(start, end) {
-    const date = new Date(
-      start.getTime() + Math.random() * (end.getTime() - start.getTime())
-    );
-    return date.toISOString().split('T')[0]; 
-}
-
-
-
   function generateRandomEmployee() {
     const firstNames = [ "Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Helen", "John", "Jeanne",
     ];
@@ -33,12 +24,17 @@ function App() {
     const departments = ["Sales", "Marketing", "Engineering", "Human Resources", "Legal"];
     const cities = [ "New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "San Francisco", "Detroit", "Whashington", "Boston", "Miami", ];
     const states = [ "Illinois", "California", "Texas", "Florida", "Arizona", "Hawaii", "Wyoming", "Idaho", "Guam", "Delaware", ];
+    const dateOfBirth = ['1978-06-08', '2000-01-01', '1972-01-25', '2002-02-02', '2003-03-17' ]
+    const startDate = [ '2003-03-03', '2000-01-15', '2004-04-04', '2004-04-04',  '2005-05-05' ]
+
     const randomFirstName =
       firstNames[Math.floor(Math.random() * firstNames.length)];
     const randomLastName =
       lastNames[Math.floor(Math.random() * lastNames.length)];
-      const randomDateOfBirth = generateRandomDate(new Date(1970, 0, 1), new Date(2000, 11, 31));
-      const randomStartDate = generateRandomDate(new Date(2000, 0, 1), new Date());
+
+      const randomDateOfBirth = dateOfBirth[Math.floor(Math.random() * dateOfBirth.length)];
+      const randomStartDate = startDate[Math.floor(Math.random() * startDate.length)];
+
     const randomStreet = `${Math.floor(Math.random() * 1000) + 1} Main St`;
     const randomCity = cities[Math.floor(Math.random() * cities.length)];
     const randomState = states[Math.floor(Math.random() * states.length)];
